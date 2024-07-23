fad20b79f7a939693e61ab83cc1ce349b912981b
Bruno Hanai <bruno.hanai@gmail.com>
2024-07-23 19:08:34 -0300

    Adiciona placeholder-data.js com conteudos de posts para auxiliar o desenvolvimento

---
+++ src/lib/placeholder-data.js
@@ -0,0 +1,24 @@
+const posts = [
+    {
+        id: "post-1",
+        created_at: "2024-07-23T19:03:00Z",
+        updated_at: NaN,
+        title: "Some title - 1",
+        headline: "Some headline - 1",
+        content: "Some long content\nwith\nbreaklines - 1",
+        status: "Published", //TODO: Enum
+        category: NaN, // TODO: Enum?
+        tags: NaN // TODO: Enum?
+    },
+    {
+        id: "post-2",
+        created_at: "2024-07-23T19:05:00Z",
+        updated_at: NaN,
+        title: "Some title - 2",
+        headline: "Some headline - 2",
+        content: "Some long content\nwith\nbreaklines - 2",
+        status: "Draft", //TODO: Enum
+        category: NaN, // TODO: Enum?
+        tags: NaN // TODO: Enum?
+    }
+]
